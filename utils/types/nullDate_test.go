56e2b15b5e31406f9d1eaf972552c68a65c43588
Andrey P <posandrey@gmail.com>
2018-10-23 11:46:14 +0200

    Added null date test

---
+++ utils/types/nullDate_test.go
@@ -0,0 +1,28 @@
+package types
+
+import (
+	"github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestJsonConversion(t *testing.T) {
+	expectedTime, err := time.Parse("2006-01-02T15:04:05", "2001-01-01T11:00:00")
+	assert.NoError(t, err)
+
+	nullTime := NullTime{
+		NullTime: mysql.NullTime{expectedTime, false},
+	}
+
+	jsonTime, err := nullTime.MarshalJSON()
+	assert.NoError(t, err)
+	assert.Equal(t, "null", string(jsonTime))
+
+	nullTime = NullTime{
+		NullTime: mysql.NullTime{expectedTime, true},
+	}
+	jsonTime, err = nullTime.MarshalJSON()
+	assert.NoError(t, err)
+	assert.Equal(t, `"2001-01-01T11:00:00Z"`, string(jsonTime))
+}
