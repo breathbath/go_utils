ada84226b3192acb43e38672d318625d69ce22ac
Andrey P <posandrey@gmail.com>
2018-10-31 19:33:33 +0100

    Added time param extractor from request

--- utils/http/requestProcessor.go
+++ utils/http/requestProcessor.go
@@ -1,9 +1,13 @@
 package http
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 )
+
+const REQUEST_TIME_FORMAT = "2006-01-02T15-04-05"
 
 func GetRequestValueString(req *http.Request, key, defaultValue string) string {
 	val, ok := req.URL.Query()[key]
@@ -26,4 +30,27 @@ func GetRequestValueInt(req *http.Request, key string, defaultValue int64) int64
 	}
 
 	return defaultValue
+}
+
+func GetRequestValueTimeWithError(req *http.Request, key string) (time.Time, error) {
+	val, ok := req.URL.Query()[key]
+	if !ok {
+		return time.Time{}, fmt.Errorf("No time value provided for key %s", key)
+	}
+
+	timeResult, err := time.Parse(REQUEST_TIME_FORMAT, val[0])
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return timeResult, nil
+}
+
+func GetRequestValueTime(req *http.Request, key string, defaultValue time.Time) time.Time {
+	timeVal, err := GetRequestValueTimeWithError(req, key)
+	if err != nil {
+		return defaultValue
+	}
+
+	return timeVal
 }
--- utils/http/requestProcessor_test.go
+++ utils/http/requestProcessor_test.go
@@ -5,13 +5,17 @@ import (
 	"net/http"
 	"net/url"
 	"testing"
+	"time"
 )
 
 var reqExample *http.Request
+var reqExampleWrong *http.Request
 
 func init() {
-	u, _ := url.Parse("http://ya.ru?someKey=someVal&someNumb=-123")
-	reqExample = &http.Request{URL: u}
+	correctUrl, _ := url.Parse("http://ya.ru?someKey=someVal&someNumb=-123&time=2001-01-01T11-00-00")
+	wrongUrl, _ := url.Parse("http://ya.ru?time=2001-01-01")
+	reqExample = &http.Request{URL: correctUrl}
+	reqExampleWrong = &http.Request{URL: wrongUrl}
 }
 
 func TestGetRequestValueString(t *testing.T) {
@@ -28,4 +32,29 @@ func TestGetRequestValueInt(t *testing.T) {
 
 	actualVal = GetRequestValueInt(reqExample, "someNumb", 0)
 	assert.EqualValues(t, -123, actualVal)
+}
+
+func TestGetRequestValueTimeWithError(t *testing.T) {
+	actualTime , err := GetRequestValueTimeWithError(reqExample, "time")
+	assert.NoError(t, err)
+
+	expectedTime, err := time.Parse("2006-01-02T15:04:05", "2001-01-01T11:00:00")
+	assert.NoError(t, err)
+
+	assert.Equal(t, expectedTime.UTC(), actualTime)
+
+	_, err = GetRequestValueTimeWithError(reqExampleWrong, "time")
+	assert.EqualError(t, err, `parsing time "2001-01-01" as "2006-01-02T15-04-05": cannot parse "" as "T"`)
+}
+
+func TestGetRequestValueTimeWithDefaultValue(t *testing.T) {
+	defaultValue, err := time.Parse("2006-01-02T15:04:05", "2002-02-02T12:02:02")
+	assert.NoError(t, err)
+
+	expectedTime, err := time.Parse("2006-01-02T15:04:05", "2001-01-01T11:00:00")
+	actualTime := GetRequestValueTime(reqExample, "time", defaultValue)
+	assert.Equal(t, expectedTime, actualTime)
+
+	actualTime = GetRequestValueTime(reqExampleWrong, "time", defaultValue)
+	assert.Equal(t, defaultValue, actualTime)
 }
