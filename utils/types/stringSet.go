68d59340a7c1a2966f7fa7d1d9ef470b9fd27f2a
Andrey P <posandrey@gmail.com>
2019-01-17 15:03:47 +0100

    Added value set

---
+++ utils/types/stringSet.go
@@ -0,0 +1,58 @@
+package types
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"strings"
+	"fmt"
+)
+
+type StringSet [] string
+
+func (vs *StringSet) Add(value string) {
+	*vs = append(*vs, value)
+}
+
+func (vs *StringSet) MarshalJSON() ([]byte, error) {
+	return json.Marshal(vs.ToStrings())
+}
+
+func (vs StringSet) ToStrings() (result []string) {
+	return []string(vs)
+}
+
+func (vs StringSet) String() string {
+	return strings.Join(vs, ",")
+}
+
+func (vs *StringSet) UnmarshalJSON(jsonInput []byte) error {
+	var items []string
+	err := json.Unmarshal(jsonInput, &items)
+	*vs = items
+	return err
+}
+
+func (vs *StringSet) Scan(value interface{}) error {
+	switch value.(type) {
+	case string:
+		vs.setFromString(value.(string))
+	case []byte:
+		vs.setFromString(string(value.([]byte)))
+	default:
+		return fmt.Errorf("Unknown value type for ValueSet: %v", value)
+	}
+
+	return nil
+}
+
+func (vs *StringSet) setFromString(rawString string) {
+	if rawString == "" {
+		return
+	}
+
+	*vs = strings.Split(rawString, ",")
+}
+
+func (vs StringSet) Value() (driver.Value, error) {
+	return vs.String(), nil
+}
---
+++ utils/types/stringSet_test.go
@@ -0,0 +1,72 @@
+package types
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStringSetAdding(t *testing.T) {
+	ss := &StringSet{}
+	ss.Add("one")
+	ss.Add("two")
+
+	assert.EqualValues(t, "one,two", ss.String())
+}
+
+func TestStringSetMarshalJson(t *testing.T) {
+	ss := &StringSet{"one", "two"}
+	jsonSs, err := ss.MarshalJSON()
+
+	assert.NoError(t, err)
+
+	assert.EqualValues(t, `["one","two"]`, jsonSs)
+}
+
+func TestStringSetToStringsConversion(t *testing.T) {
+	ss := &StringSet{"one", "two"}
+
+	assert.EqualValues(t, []string{"one", "two"}, ss.ToStrings())
+}
+
+func TestStringSetUnMarshalJson(t *testing.T) {
+	ssJson := []byte(`["one","two"]`)
+
+	providedSs := &StringSet{}
+	err := providedSs.UnmarshalJSON(ssJson)
+	assert.NoError(t, err)
+
+	expectedSs := &StringSet{"one", "two"}
+
+	assert.EqualValues(t, expectedSs, providedSs)
+}
+
+func TestStringSetScan(t *testing.T) {
+	providedSs := &StringSet{}
+
+	err := providedSs.Scan("")
+	assert.NoError(t, err)
+	assert.EqualValues(t, &StringSet{}, providedSs)
+
+	err = providedSs.Scan("one,two")
+	assert.NoError(t, err)
+	assert.EqualValues(t, &StringSet{"one", "two"}, providedSs)
+
+	notSplitableInputs := []string{"one.two", "one two", "one-two", "one"}
+	for _, notSplitableInput := range notSplitableInputs {
+		err = providedSs.Scan(notSplitableInput)
+		assert.NoError(t, err)
+		assert.EqualValues(t, &StringSet{notSplitableInput}, providedSs)
+	}
+
+	err = providedSs.Scan([]byte("one,two"))
+	assert.NoError(t, err)
+	assert.EqualValues(t, &StringSet{"one", "two"}, providedSs)
+}
+
+func TestStringSetValueConversion(t *testing.T) {
+	ss := &StringSet{"one", "two"}
+	val, err := ss.Value()
+	assert.NoError(t, err)
+
+	assert.Equal(t, "one,two", val)
+}
